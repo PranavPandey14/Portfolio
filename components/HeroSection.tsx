ca72801f8e527e02830271ce7703d837026d63c9
Pranav Pandey <pranavpandey12245@gmail.com>
2023-12-31 04:11:51 +0530

    bug fixes

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,10 +1,10 @@
 "use client"; // this is a client component
-import React,{ReactNode} from "react";
+import React from "react";
 import Image from "next/image";
 import { Link } from "react-scroll/modules";
 import { HiArrowDown } from "react-icons/hi";
 
-const HeroSection = () : ReactNode => {
+const HeroSection = () => {
   return (
     <section id="home">
       <div className="   flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
